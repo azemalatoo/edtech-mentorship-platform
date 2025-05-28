b72947e7663bc2cde6f3dec49c94b1701876ebdf
azemalatoo <azem.kakitaeva@alatoo.edu.kg>
2025-05-28 16:37:57 +0600

    minor fixes in swagger config

--- src/main/java/alatoo/edu/edtechmentorshipplatform/config/SwaggerConfig.java
+++ src/main/java/alatoo/edu/edtechmentorshipplatform/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package alatoo.edu.edtechmentorshipplatform.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
@@ -9,20 +10,22 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info().title("Mentor Connect API")
+                .info(new Info()
+                        .title("Mentor Connect API")
                         .description("API Documentation for online mentorship platform for EdTech")
                         .version("1.0"))
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .components(new io.swagger.v3.oas.models.Components()
-                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
-                                .name("Authorization")
-                                .type(SecurityScheme.Type.APIKEY)
-                                .in(SecurityScheme.In.HEADER)
-                                .bearerFormat("JWT")
-                                .scheme("bearer")));
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
     }
 }
+
